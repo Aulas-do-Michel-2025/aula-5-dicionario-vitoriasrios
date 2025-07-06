ec3fe0c69fc08030bd0c547e39026d186f058000
vitoriasrios <vitoriasrios97@gmail.com>
2025-07-06 17:37:23 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -32,3 +32,22 @@ variantes = [
     {"id": "rs252627", "frequencia": 0.0001},
     {"id": "rs282930", "frequencia": 0.06},
 ]
+
+
+variantes = [
+    {"id": "rs789", "frequencia": 0.07},
+    {"id": "rs101112", "frequencia": 0.03},
+    {"id": "rs131415", "frequencia": 0.0005},
+    {"id": "rs161718", "frequencia": 0.05},
+    {"id": "rs192021", "frequencia": 0.09},
+    {"id": "rs222324", "frequencia": 0.012},
+    {"id": "rs252627", "frequencia": 0.0001},
+    {"id": "rs282930", "frequencia": 0.06},
+]
+
+print("As variantes raras são:")
+
+for variante in variantes:
+    if variante["frequencia"] < 0.01:
+        print(variante["id"])
+    
